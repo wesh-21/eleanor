3d9a2fcaa14c42a7fb4ddf60f9aa472aca369d11
Miguel Bibi <miguel.c.bibi@gmail.com>
2025-03-17 22:25:23 +0000

    fix: refactor Header component and implement suspense loading in Confirmation page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect, SyntheticEvent } from "react";
+import { useState, useEffect } from "react";
 
 export default function Header() {
   // Add a state to track scroll position
